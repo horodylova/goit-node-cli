e106760a59ba37da701be23efdcede722f3b0aff
svitlana-horodylova <horodylova.sv@gmail.com>
2024-02-07 17:52:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -17,21 +17,22 @@ async function invokeAction({ action, id, name, email, phone }) {
     case "list":
       const allContacts = await listContacts();
       console.log(allContacts);
-  
+      break;
 
     case "get":
       const oneContact = await getContactById(id);
       console.log(oneContact);
-   
+      break;
 
     case "add":
       const newContact = await addContact({ name, email, phone });
       console.log(newContact);
-  
+      break;
 
     case "remove":
       const deleteContact = await removeContact(id);
       console.log(deleteContact);
+      break;
 
       default:
         console.warn("Unknown action type");
